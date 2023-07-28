db5f6c2163391ca6e765faa6c5c57d8e632b88ce
35032 <35032@LAPTOP-FQC7AND3>
2023-07-28 17:15:43 +0800

    封装了更多参数，增加了注释

--- main.py
+++ main.py
@@ -33,13 +33,15 @@ if str(ROOT) not in sys.path:
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 # 参数
-device = 0  # 硬件 0：GPU
+source = 0  # 视频源 0: 电脑自带摄像头； 1： 外部摄像头
+device = 'CPU'  # 硬件 '0'：GPU 或 'CPU'：CPU
 weights = ROOT / 'vision/best.pt'  # 权重文件
 data = ROOT / 'vision/0515.yaml'  # 类别等信息
 imgsz = (640, 640)  # 新图大小
 half = False  # use FP16 half-precision inference
 dnn = False  # use OpenCV DNN for ONNX inference
 itv = 50  # 每隔多少毫秒做一次识别
+conf_thres = 0.7  # 置信度阈值
 
 device = select_device(device)
 model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data, fp16=half)
@@ -50,12 +52,12 @@ bs = 1
 model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
 
 # 加载摄像头
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(source)
 
 pre = int(time.time() * 1000)
 while True:
     if int(time.time() * 1000) == pre + itv:
         ret, im0 = cap.read()  # 截图
         im = MyLoadIamge(im0=im0, img_size=imgsz, stride=stride, auto=pt)
-        detect(im0=im0, im=im, model=model, itv=itv, conf_thres=0.7)
+        detect(im0=im0, im=im, model=model, itv=itv, conf_thres=conf_thres)
         pre = int(time.time() * 1000)
