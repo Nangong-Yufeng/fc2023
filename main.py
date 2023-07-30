b1b77a9ff87d3886247b763cd11646e56985ba5f
35032 <35032@LAPTOP-FQC7AND3>
2023-07-30 16:59:47 +0800

    更新main.py

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ vis = Vision(source=0, device='0', conf_thres=0.7)
 itv = 50  # 每次检测的间隔时间，单位ms
 pre = int(time.time() * 1000)  # 上次检测完的时间
 while True:
-    if int(time.time() * 1000) == pre + itv:
+    if int(time.time() * 1000) > pre + itv:
         vis.run()
         pre = int(time.time() * 1000)
