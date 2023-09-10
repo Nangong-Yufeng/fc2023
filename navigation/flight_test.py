c2d3b5bee443b906486fa7b1280b65bb86998299
bobo <1946561784@qq.com>
2023-09-10 17:56:13 +0800

    靶标识别对接

--- navigation/flight_test.py
+++ navigation/flight_test.py
@@ -1,6 +1,6 @@
 from pymavlink import mavutil
 from .preflight import arm, mode_set, set_home
-from .mission import wp_straight_course, wp_circle_course, yard_fly, clear_waypoint
+from .mission import wp_straight_course, wp_circle_course, yard_fly, clear_waypoint, mission_upload
 from .class_list import Position_relative, Waypoint
 from .error_process import rec_match_received, retry_fuc_para1
 from .get_para import gain_ground_speed, gain_transform_frequency
@@ -47,3 +47,38 @@ wp4 = Waypoint(22.5909266, 113.9752198, 120)
 wp = [wp1, wp2, wp3, wp4]
 
 yard_fly(the_connection, wp, home_position, track_list)
+
+
+def vision_test_court(the_connection):
+    # 已知航点(操场的四个角)
+    a = input("输入需要的环线高度（输入为空则默认为120米）： ")
+    if a == '':
+        alt = 120
+    else:
+        alt = int(a, base=10)
+    print(alt)
+
+    wp1 = Waypoint(22.5899275, 113.9751526, alt)
+    wp2 = Waypoint(22.5899248, 113.9755938, alt)
+    wp3 = Waypoint(22.5909185, 113.9755938, alt)
+    wp4 = Waypoint(22.5909266, 113.9752198, alt)
+    wp = [wp1, wp2, wp3, wp4]
+
+    # 环操场航点
+    wp_line1 = [wp[3], wp[0]]
+    wp_circle1 = [wp[0], wp[1]]
+    wp_line2 = [wp[1], wp[2]]
+    wp_circle2 = [wp[2], wp[3]]
+
+    wp_list = (wp_circle_course(wp_circle1, 3, 180, 1))
+    wp_list.pop(-1)
+    wp_list.pop(-1)
+    wp_list.extend(wp_straight_course(wp_line2, 3))
+    wp_list.pop(-1)
+    wp_list.extend(wp_circle_course(wp_circle2, 3, 180, 1))
+    wp_list.pop(-1)
+    wp_list.extend(wp_straight_course(wp_line1, 3))
+
+    mission_upload(the_connection, wp_list, home_position)
+
+    return wp_list
--- navigation/set_para.py
+++ navigation/set_para.py
@@ -3,12 +3,12 @@ import sys
 from pymavlink import mavutil
 
 #设置空速大小
-def set_speed(vehicle, desired_flight_speed):
+def set_ground_speed(vehicle, desired_flight_speed):
     message = mavutil.mavlink.MAVLink_command_long_message(target_system=vehicle.target_system,
                                                    target_component=vehicle.target_component,
                                                    command=mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED,
                                                    confirmation=0,
-                                                   param1=0,
+                                                   param1=1,
                                                    param2=desired_flight_speed,
                                                    param3=0,
                                                    param4=0,
@@ -19,8 +19,8 @@ def set_speed(vehicle, desired_flight_speed):
     vehicle.mav.send(message)
     msg = vehicle.recv_match(type="COMMAND_ACK", blocking=True)
     if msg.result == 0:
-        print("successfully set airspeed as ", (desired_flight_speed), "m/s")
+        print("successfully set ground speed as ", (desired_flight_speed), "m/s")
         return 0
     else:
-        print("airspeed set failed")
+        print("ground speed set failed")
         return -10
