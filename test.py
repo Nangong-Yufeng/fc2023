308cbc5ab2e7ed4721ce6f8ad7205a818a1dd037
bobo <1946561784@qq.com>
2023-10-13 00:40:21 +0800

    main函数迭代为k—means算法分开侦察版本

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from utils import title
 import threading
 import queue
@@ -11,7 +13,8 @@ from navigation import (Waypoint, mode_set, mission_upload,
                         wrong_number, wp_bombing_course, mission_current, bomb_drop,
                         loiter, return_to_launch, initiate_bomb_drop, preflight_command
                         ,wp_detect_course_HeBei, wp_detect_course_HeBei_2g, force_arm,
-                        gain_transform_frequency, k_means_calculate, target_point)
+                        gain_transform_frequency, k_means_calculate, target_point, target_order,
+                        target_match, coordinate_aver_cal)
 from pymavlink import mavutil
 # 目标字典的目标存储个数
 LEN_OF_TARGET_LIST = 50
@@ -25,10 +28,32 @@ wp_start = Waypoint(38.5590428, 115.1420812, 15)  # A组，顺时针侦察
 final_target_position = Waypoint(38.5592552, 115.1421690, 0)
 mission_start_time = 0
 
+key = [5,7,9]
+print(np.median(key))
+
 A = target_point(0.0001, 0.0002, 1)
 B = target_point(0.0001, 0.0002, 2)
 C = target_point(0.0002, 0.0001,2)
 
+target_list = [Waypoint(-35.3603933, 149.1602209, 1),
+               Waypoint(-35.3605727, 149.1600707, 2),
+               Waypoint(-35.3605716, 149.1603376, 3)]
+target_num = [Waypoint(-35.3605563, 149.1602477, 6),
+              Waypoint(0, 0, 6),
+              Waypoint(0, 0, 6)]
+              # Waypoint(-35.3603507, 149.1602598, 5),
+              # Waypoint(-35.3606438, 149.1599809, 4)]
+
+points = [target_point(0.1,0.8,1),
+          target_point(0.9,0.4,1),
+          target_point(14,3,5),
+          target_point(0.7, 0.5, 1),
+          target_point(0.3, 0.7, 1)]
+coordinate_aver_cal(points,1)
+
+
+a = target_match(target_list, target_num, 0)
+print(a.lat, a.lon)
 a = k_means_calculate([A, B, C])
 '''
 the_connection = mavutil.mavlink_connection('/dev/ttyUSB0', baud=921600)#921600)
